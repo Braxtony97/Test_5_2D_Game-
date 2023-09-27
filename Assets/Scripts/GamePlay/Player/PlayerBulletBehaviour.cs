995a6ed5a446731892afe0c9c3a4854feb081c81
Renat <renat_samigullin_97@mail.ru>
2023-09-28 00:50:12 +0500

    Реализована стрельба по врагу

--- Assets/Scripts/GamePlay/Player/PlayerBulletBehaviour.cs
+++ Assets/Scripts/GamePlay/Player/PlayerBulletBehaviour.cs
@@ -5,10 +5,24 @@ namespace Assets.Scripts.GamePlay.Player
     public class PlayerBulletBehaviour : MonoBehaviour
     {
         private float _bulletSpeed = 5f;
+        private Transform _targetEnemy;
+
+        public void SetTareget(Transform TargetEnemy)
+        {
+            _targetEnemy = TargetEnemy;
+        }
 
         private void Update()
         {
-            transform.Translate(Vector2.right * _bulletSpeed * Time.deltaTime);
+            if (_targetEnemy != null)
+            {
+                Vector2 direction = (_targetEnemy.position - transform.position).normalized;
+                transform.Translate(direction * _bulletSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector2.right * _bulletSpeed * Time.deltaTime);
+            }
         }
     }
 }
